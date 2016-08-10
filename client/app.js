38afe0f76dc7e00010a4895c0f4954cfce1920e8
Jays0nnn <jaypelayo037@yahoo.com>
2016-08-10 18:56:49 -0400

    tidied my controller up, put everything in a service. Although I cant seem to get my service file to link up with my appjs file

--- client/app.js
+++ client/app.js
@@ -11,68 +11,29 @@ app.config(function($stateProvider, $urlRouterProvider){
 		url: '/about',
 		templateUrl: 'templates/about.html'
 	})
-	.state('blog', {
-		url: '/blog',
-		templateUrl: 'templates/blog.html'
+	.state('resume',{
+		url: '/resume',
+		templateUrl: 'templates/resume.html'
 	});
 
 });
 
 
-app.controller('navCtrl' , function() {
+app.controller('navCtrl', function(nav) {
 
-	var obj = this;
-
-	obj.navigations = {
-
-		
-		key1 :'WORK',
-		key2 :'BLOG',
-		key3 : 'ABOUT'
-		
-	};
-
-
-});
-
-app.controller('mainCtrl', function() {
-
-
-	var obj = this;
-
-	obj.strings = {
-
-		key1 : 'Developer, Graphic Designer, Foodie, & Exercise Fanatic',
-		key2 : 'Suh dude! Im Van Jayson Pelayo, a self taught developer & junior graphic designer with two years of design work experience. I\'m currently an Developer intern at Metro Click, while taking on freelance graphic design projects on the side. I\'m currently based in Connecticut.',
-		title : 'RECENT PROJECTS'
-	};
+	
 });
 
 
-app.directive('navDir', function () {
 
-	return {
-		restrict : 'E',
-		controller : 'navCtrl',
-		templateUrl : 'templates/icon.html'
-	};
-});
 
-app.controller('imgCtrl', function() {
+// app.directive('navDir', function () {
 
-	var obj = this;
+// 	return {
+// 		restrict : 'E',
+// 		controller : 'navCtrl',
+// 		templateUrl : 'templates/icon.html'
+// 	};
+// });
 
-	obj.imgs = {
 
-		img1 : 'one',
-		img2 : 'two',
-		img3 : 'three',
-		img4 : 'four',
-		img5 : 'five',
-		img6 : 'six',
-		img7 : 'seven',
-		img8 : 'eight',
-		img9 : 'nine'
-
-	};
-});
