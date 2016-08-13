b9a5a7c94eac6fb22eba85b3a7cf30237149e49d
Jays0nnn <jaypelayo037@yahoo.com>
2016-08-12 21:19:22 -0400

    New Layout, added custom Directives

--- client/app.js
+++ client/app.js
@@ -19,12 +19,47 @@ app.config(function($stateProvider, $urlRouterProvider){
 });
 
 
+app.directive('headDir', function() {
+
+	return {
+
+		restrict: 'E',
+		templateUrl: 'templates/head.html'
+
+	};
+});
+
+
+
 app.controller('navCtrl', function(nav) {
+
+	var obj = this;
+
+	obj.strings = {
+
+		key1 : 'Front End Developer, Graphic Designer, Foodie, & Exercise Fanatic',
+		key2 : 'Suh dude! Im Van Jayson Pelayo, a self taught developer & junior graphic designer with two years of design work experience. I\'m currently an Developer intern at Metro Click, while taking on freelance graphic design projects on the side. I\'m currently based in Connecticut.',
+		title : 'RECENT PROJECTS'
+	};
+
+	obj.content = {
+
+		con_1 : 'Shoe Shine',
+		con_2 : 'Brick & Ivy'
+	};
 
 	
 });
 
 
+
+
+
+app.service('nav', function () {
+
+
+
+});
 
 
 // app.directive('navDir', function () {
