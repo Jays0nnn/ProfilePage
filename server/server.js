cd31eb8c35cfa9f502e9b34e04120d3de166dcce
Jays0nnn <jaypelayo037@yahoo.com>
2016-08-16 11:41:54 -0400

    ...:

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 
-var main = app.use('/', express.static('/client'));
+var main = app.use('/', express.static('../client'));
 
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname +'/index.html'));
@@ -15,5 +15,5 @@ app.get('/', function(req,res) {
 
 app.listen(8000, function(){
 
-	console.log('Go mangg');
+	console.log('running on 8000');
 });
