c03720796fa094fa58c212f5b7a5648d988cfe2e
Jays0nnn <jaypelayo037@yahoo.com>
2016-08-16 02:16:10 -0400

    fuck

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 
-var main = app.use('/', express.static('../client'));
+var main = app.use('/', express.static('/client'));
 
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname +'/index.html'));
