0e1c54a16677e2b33f9b5ddfb5db1132fd4af5af
Jays0nnn <jaypelayo037@yahoo.com>
2016-08-18 17:36:49 -0400

    fixed layout & added homepage animations

--- client/app.js
+++ client/app.js
@@ -19,27 +19,15 @@ app.config(function($stateProvider, $urlRouterProvider){
 });
 
 
-app.directive('headDir', function($parse) {
-
-	function introFunction (scope,elem,attr) {
-
-		// var obj = $parse(attr.headDir);
-		// console.log(obj);
-
-		elem.bind('load', function(){
-			console.log('woo it works!');
-		
-		});
-	}
+app.directive('headDir', function() {
 
 	return {
 
 		restrict: 'E',
-		templateUrl: 'templates/head.html',
-		link: introFunction
+		templateUrl: 'templates/head.html'
 
-	};
-});
+		}
+	});
 
 app.directive('contDir', function() {
 
@@ -47,21 +35,81 @@ app.directive('contDir', function() {
 
 		restrict: 'E',
 		templateUrl: 'templates/content.html',
+		// link: function(scope,elem,attr){
+		// 	elem.bind('click', function(){
+		// 		console.log('sofarsogood');
+		// 		$(this).replaceWith('.test');
+		// 		$(".home").addClass("test");
+		// 		$(".home").click(function(){
+		// 			elem.bind('click', function(){
+		// 				console.log('thisworks!');
+		// 			})
+		// 			$('.about').slideUp('moderate');
+
+		// 		});
+		// 	});
+		// }
+	};
+});
+
+app.directive('pro1', function(){
+	return {
+
+		restrict: 'E',
+		templateUrl: "templates/pro1.html",
 		link: function(scope,elem,attr){
 			elem.bind('click', function(){
 				console.log('sofarsogood');
-				// $(this).replaceWith('.test');
-				// $(".home").addClass("test");
-				$(".home").click(function(){
-					// elem.bind('click', function(){
-					// 	console.log('thisworks!');
-					// })
-					$('.about').slideUp('moderate');
-
-				});
 			});
+
 		}
 	};
+
+});
+
+app.directive('pro2', function(){
+	return {
+
+		restrict: 'E',
+		templateUrl: "templates/pro2.html",
+		link: function(scope,elem,attr){
+			elem.bind('click', function(){
+				console.log('project2 good!');
+			});
+
+		}
+	};
+
+});
+
+app.directive('pro3', function(){
+	return {
+
+		restrict: 'E',
+		templateUrl: "templates/pro3.html",
+		link: function(scope,elem,attr){
+			elem.bind('click', function(){
+				console.log('project3 good!');
+			});
+
+		}
+	};
+
+});
+
+app.directive('pro4', function(){
+	return {
+
+		restrict: 'E',
+		templateUrl: "templates/pro4.html",
+		link: function(scope,elem,attr){
+			elem.bind('click', function(){
+				console.log('project4 good!');
+			});
+
+		}
+	};
+
 });
 
 
